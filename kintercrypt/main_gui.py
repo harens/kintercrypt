51d3742de0fabeaab2fd98c930b26c0f7c1afbca
harens <harensdeveloper@gmail.com>
2020-06-12 19:03:00 +0100

    Encryption Process Added

--- kintercrypt/main_gui.py
+++ kintercrypt/main_gui.py
@@ -19,11 +19,14 @@
 This module sets up the GUI for kintercrypt, including the various tkinter widgets and properties
 """
 
+from pathlib import Path
+import os
 from tkinter.filedialog import askopenfilename
 from tkinter.scrolledtext import ScrolledText
 from tkinter import ttk
 import tkinter as tk
-from time import ctime
+from time import ctime, time
+from kintercrypt.ciphers.cipher_manager import main_cipher
 
 
 # Adapted class structure from https://www.begueradj.com/tkinter-best-practices/
@@ -39,13 +42,14 @@ class App(tk.Frame):  # pylint: disable=too-many-ancestors
         height: Minimum possible window height
 
         file: The file to be encrypted/decrypted
+        password: Password to encrypt/decrypt the file
 
         note: The notebook interface
         tab1: General tab
-        tab2: Settings tab
-        tab3: About tab
+        tab2: Cipher tab
 
         output_area: Outputs diagnostic results of encryption
+        initial_value: Initial value of the encrypt/decrypt dropdown menu
 
     """
 
@@ -61,25 +65,35 @@ class App(tk.Frame):  # pylint: disable=too-many-ancestors
         self.width = 400
         self.height = 200
 
-        # File to be encrypted/decrypted
+        # File to be encrypted/decrypted and password
         self.file = ""
+        self.password = ""
 
         # Create the tabs
         self.note = ttk.Notebook(self.parent)
         self.tab1 = ttk.Frame(self.note)
         self.tab2 = ttk.Frame(self.note)
-        self.tab3 = ttk.Frame(self.note)
 
         # Assign tab titles
         self.note.add(self.tab1, text="General")
-        self.note.add(self.tab2, text="Settings")
-        self.note.add(self.tab3, text="About")
+        self.note.add(self.tab2, text="Cipher")
 
         self.note.pack(fill=tk.BOTH, expand=True)
 
         # Outputs diagnostic results of encryption
-        # It's not in an "add widgets" method since it needs to be accessed by other methods
+        # The following properties are not in the "add widgets" method
+        # This is since it needs to be accessed by other methods
         self.output_area = ScrolledText(self.tab1, wrap=tk.WORD)
+
+        # Choose between encrypt and decrypt
+        # Based off http://effbot.org/tkinterbook/optionmenu.htm
+        self.initial_value = tk.StringVar(self.tab1)
+
+        # Password Input
+        self.password_entry = ttk.Entry(self.tab1, show="*")
+        self.password_entry.grid(
+            row=0, column=1, columnspan=3, sticky="WE"
+        )  # Seperate grid prevents password_entry from being None
 
         # Methods to arrange the app
         self.configure_app()
@@ -125,28 +139,26 @@ class App(tk.Frame):  # pylint: disable=too-many-ancestors
         # Password Label
         ttk.Label(self.tab1, text="Password:").grid(row=0, column=0)
 
-        # Password Input
-        ttk.Entry(self.tab1, show="*").grid(row=0, column=1, columnspan=3, sticky="WE")
-
         # Allows the buttons to be in the same grid cell
         button_area = ttk.Frame(self.tab1)
         button_area.grid(row=1, column=0)
 
-        # Choose between encrypt and decrypt
-        # Based off http://effbot.org/tkinterbook/optionmenu.htm
-        initial_value = tk.StringVar(self.tab1)
-        initial_value.set("Encrypt")
-
-        option_menu = ttk.OptionMenu(button_area, initial_value, "Encrypt", "Decrypt")
+        self.initial_value.set("Encrypt")
+        # Encrypt twice since otherwise, decypt is the only option
+        option_menu = ttk.OptionMenu(
+            button_area, self.initial_value, "Encrypt", "Encrypt", "Decrypt"
+        )
         option_menu.grid(
             row=0, column=0
-        )  # Seperate grid so that the widget isn't assigned as None
+        )  # Separate grid so that the widget isn't assigned as None
 
         ttk.Button(button_area, text="Choose file", command=self.choose_file).grid(
             row=1, column=0, sticky="WE", pady=10
         )  # Padding for the middle button spaces all buttons
 
-        ttk.Button(button_area, text="Start").grid(row=2, column=0, sticky="WE")
+        ttk.Button(button_area, text="Start", command=self.start_cipher).grid(
+            row=2, column=0, sticky="WE"
+        )
 
         self.output_area.grid(row=1, column=1, columnspan=3, sticky="WE")
 
@@ -158,7 +170,47 @@ class App(tk.Frame):  # pylint: disable=too-many-ancestors
         if not self.file:  # If file selector is opened, but no file is chosen
             self.log_output("No file chosen")
         else:
-            self.log_output(str(self.file) + " chosen")
+            # Basename only outputs the file name, not the path
+            self.log_output(f"{Path(self.file).stem} chosen")
+
+    def start_cipher(self) -> None:
+        """Performs various checks before encryption begins
+
+        After performing the checks, it retrieves the encrypted/decrypted text
+        It then writes it to a new file
+
+        """
+        if not self.file:
+            self.log_output("ERROR: File not chosen")
+            return
+
+        self.password = self.password_entry.get()
+
+        if not self.password:
+            self.log_output("ERROR: Password not set")
+            return
+
+        # Opens the chosen file as read only
+        with open(self.file, "r") as chosen_file:
+            file_contents = chosen_file.read()
+            if not os.path.getsize(self.file):
+                self.log_output("ERROR: File is empty")
+                return
+
+            cipher_choice = (
+                self.initial_value.get()
+            )  # Whether the user wants to encrypt or decrypt
+
+            # Encrypt -> Encryption, etc.
+            self.log_output(f"{cipher_choice}ion started")
+
+            final_result = main_cipher(
+                file_contents, self.password, "XOR", cipher_choice
+            )
+
+        with open(self.file, "w") as result_file:
+            result_file.write(final_result)
+            self.log_output(f"{cipher_choice}ion Finished!")
 
 
 def main() -> None:
